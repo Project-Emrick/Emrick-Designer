54f97a83285f4c4f855bca4c5cca12cec2411fb6
nnabiyev01 <nabiyev2000@gmail.com>
2024-02-12 18:20:20 -0500

    enhanced Unzip.java

--- project/src/Unzip.java
+++ project/src/Unzip.java
@@ -6,37 +6,36 @@ import java.util.zip.*;
 
 public class Unzip {
     public static void unzip(String archiveSrc, String archiveDest) {
-        File dest = new File(archiveDest);
-        if (!dest.exists()) {
-            dest.mkdirs();
+        File destDir = new File(archiveDest);
+        if (!destDir.exists()) {
+            destDir.mkdirs();
         }
 
-        FileInputStream fis;
+        try (FileInputStream fis = new FileInputStream(archiveSrc);
+             ZipInputStream zis = new ZipInputStream(fis)) {
 
-        byte[] buffer = new byte[1024];
-        try {
-            fis = new FileInputStream(archiveSrc);
-            ZipInputStream zis = new ZipInputStream(fis);
             ZipEntry ze = zis.getNextEntry();
-            while(ze != null){
+            while (ze != null) {
                 String fileName = ze.getName();
                 File newFile = new File(archiveDest + File.separator + fileName);
-                new File(newFile.getParent()).mkdirs();
-                FileOutputStream fos = new FileOutputStream(newFile);
-                int len;
-                while ((len = zis.read(buffer)) > 0) {
-                    fos.write(buffer, 0, len);
+                new File(newFile.getParent()).mkdirs(); // Ensure parent directories exist
+
+                try (FileOutputStream fos = new FileOutputStream(newFile)) {
+                    byte[] buffer = new byte[1024];
+                    int len;
+                    while ((len = zis.read(buffer)) > 0) {
+                        fos.write(buffer, 0, len);
+                    }
+                } catch (IOException e) {
+                    System.err.println("Error extracting file: " + fileName);
+                    e.printStackTrace();
                 }
-                fos.close();
-                //close this ZipEntry
-                zis.closeEntry();
+
                 ze = zis.getNextEntry();
             }
-            //close last ZipEntry
-            zis.closeEntry();
-            zis.close();
-            fis.close();
+
         } catch (IOException e) {
+            System.err.println("Error processing zip file: " + archiveSrc);
             e.printStackTrace();
         }
     }
