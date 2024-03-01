f1830f9d96e4b6f909917b433f2f5fb597c90bda
Alex Bolinger <39972127+Alex-Bolinger@users.noreply.github.com>
2024-03-01 12:36:18 -0500

    fixed board reset bug

--- src/main/java/org/emrick/project/SerialTransmitter.java
+++ src/main/java/org/emrick/project/SerialTransmitter.java
@@ -19,6 +19,8 @@ public class SerialTransmitter {
     }
 
     public void writeToSerialPort(String str) {
+        sp.clearRTS();
+        sp.clearDTR();
         if (!sp.openPort()) {
             System.out.println("Port is busy");
         }
