61cef3839998690cd2df44429a083fd819514d11
PPancion <peterpancione10@gmail.com>
2024-04-24 20:19:22 -0400

    Added User Login and Auth

--- src/main/java/org/emrick/project/MediaEditorGUI.java
+++ src/main/java/org/emrick/project/MediaEditorGUI.java
@@ -25,7 +25,7 @@ import com.itextpdf.text.Document;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfWriter;
 
-public class MediaEditorGUI extends Component implements ImportListener, ScrubBarListener, SyncListener, FootballFieldListener, EffectListener, SelectListener {
+public class MediaEditorGUI extends Component implements ImportListener, ScrubBarListener, SyncListener, FootballFieldListener, EffectListener, SelectListener, UserAuthListener {
 
     // String definitions
     public static final String FILE_MENU_NEW_PROJECT = "New Project";
@@ -673,6 +673,18 @@ public class MediaEditorGUI extends Component implements ImportListener, ScrubBa
 //            }
         });
 
+        JMenuItem loginItem = new JMenu("Account");
+        //loginItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_L, ActionEvent.CTRL_MASK));
+        menuBar.add(loginItem);
+
+        JMenuItem signIn = new JMenuItem("Sign In");
+        signIn.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, ActionEvent.CTRL_MASK));
+        signIn.addActionListener(e -> {
+            System.out.println("Signing in...");
+            new UserAuthGUI(frame, this); // This assumes UserAuthGUI sets itself visible
+        });
+        loginItem.add(signIn);
+
         // System message
         menuBar.add(Box.createHorizontalGlue());
         menuBar.add(sysMsg);
@@ -1248,7 +1260,10 @@ public class MediaEditorGUI extends Component implements ImportListener, ScrubBa
     }
 
     ////////////////////////// Effect Listeners //////////////////////////
-
+    @Override
+    public void onUserLoggedIn(String username) {
+        frame.setTitle("Emrick Designer - Welcome "+username);
+    }
     @Override
     public void onCreateEffect(Effect effect) {
         boolean successful = this.effectManager.addEffectToSelectedPerformer(effect);
