b1f5775442bd5aefb71fbfa6bf6690c59f135692
Alex Bolinger <39972127+Alex-Bolinger@users.noreply.github.com>
2024-06-23 15:05:34 -0400

    Windows decided it doesn't like generating shortcuts automatically anymore

--- build.bat
+++ build.bat
@@ -1,10 +1,10 @@
 @echo off
-SET VERSION="6.17.0"
+SET VERSION="6.23.0"
 SET DIR=".\build\dist\\%VERSION%\Emrick Designer"
 SET NAME="Emrick Designer"
 call .\gradlew.bat clean
 call .\gradlew.bat jpackage
 call xcopy /s/e .\src ""%DIR%"\src\" > nul
 echo Files Copied
-call jpackage --type exe --app-image %DIR% --name %NAME% --app-version "%VERSION%" -d ".\build\dist" --file-associations "FAemrick.properties" --file-associations "FApacket.properties" --win-per-user-install --win-dir-chooser --win-shortcut
+call jpackage --type exe --app-image %DIR% --name %NAME% --app-version "%VERSION%" -d ".\build\dist" --file-associations "FAemrick.properties" --file-associations "FApacket.properties" --win-per-user-install --win-dir-chooser
 echo BUILD COMPLETE
