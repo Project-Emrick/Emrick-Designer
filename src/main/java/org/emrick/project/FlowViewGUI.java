9e58307867503bfc8e7ff77aef1cf457688f2f6e
Alex Bolinger <39972127+Alex-Bolinger@users.noreply.github.com>
2024-09-10 12:04:22 -0400

    Completed flow viewer update

--- src/main/java/org/emrick/project/FlowViewGUI.java
+++ src/main/java/org/emrick/project/FlowViewGUI.java
@@ -5,8 +5,12 @@ import org.emrick.project.effect.RFTrigger;
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
+import java.awt.event.AdjustmentEvent;
+import java.awt.event.AdjustmentListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
@@ -19,21 +23,40 @@ public class FlowViewGUI extends JPanel {
     private JPanel scrollablePanel;
     private ArrayList<JPanel> flowViewPanels;
     int currentTrigger;
+    private JPanel headerPanel;
+    private JLabel indexLabel = new JLabel("Index");
+    private JLabel countLabel = new JLabel("Count");
+    private JLabel titleLabel = new JLabel("Title");
+    private JLabel descriptionLabel = new JLabel("Description");
+    private JLabel cueLabel = new JLabel("Cue");
+    private JLabel executeLabel = new JLabel("Execute");
+    private boolean started;
 
     public FlowViewGUI(HashMap<Integer, RFTrigger> count2RFTrigger, RFSignalListener rfSignalListener) {
         this.rfSignalListener = rfSignalListener;
         Iterator<RFTrigger> iterator = count2RFTrigger.values().iterator();
         currentTrigger = 0;
+        started = false;
         this.scrollablePanel = new JPanel();
         scrollablePanel.setBackground(new Color(0, 0, 0, 0));
         this.setLayout(new BoxLayout(this,BoxLayout.Y_AXIS)); //new layout
+        this.setFocusable(true);
+        this.addPropertyChangeListener(e -> {
+            if (this.isShowing()) {
+                this.requestFocusInWindow();
+            }
+        });
 
         this.addKeyListener(new KeyListener() { //When spacebar is hit, move to next trigger
             @Override
             public void keyTyped(KeyEvent e) {
-                System.out.println(++currentTrigger);
-                rfSignalListener.onRFSignal(currentTrigger);
-                System.out.println("Current Trigger: " + currentTrigger);
+                if (e.getKeyChar() == ' ') {
+                    started = true;
+                    rfSignalListener.onRFSignal(currentTrigger % items.size());
+                    System.out.println("Current Trigger: " + currentTrigger % items.size());
+                    currentTrigger++;
+                    reinitializeFlowViewPanel();
+                }
             }
 
             @Override
@@ -45,7 +68,7 @@ public class FlowViewGUI extends JPanel {
         int i = 0;
         while(iterator.hasNext()) {
             RFTrigger curr = iterator.next();
-            items.add(new FlowViewItem(i, curr.getCount(), "", "", ""));
+            items.add(new FlowViewItem(i, curr.getCount(), "placeholder", "placeholder", "placeholder"));
             i++;
         }
         items.sort(Comparator.comparingInt(FlowViewItem::getCount));
@@ -63,34 +86,67 @@ public class FlowViewGUI extends JPanel {
         Border innerBorder = BorderFactory.createTitledBorder("Flow View");
         Border outerBorder = BorderFactory.createEmptyBorder(5, 5, 5, 5);
         scrollablePanel.setBorder(BorderFactory.createCompoundBorder(outerBorder, innerBorder));
+        headerPanel = new JPanel();
+        headerPanel.setLayout(new BoxLayout(headerPanel, BoxLayout.X_AXIS));
+        headerPanel.setPreferredSize(new Dimension(800, 50));
+        headerPanel.setMaximumSize(new Dimension(800, 50));
+        headerPanel.setMinimumSize(new Dimension(800, 50));
+        indexLabel.setMinimumSize(new Dimension(133, 30));
+        indexLabel.setPreferredSize(new Dimension(133, 30));
+        indexLabel.setMaximumSize(new Dimension(133, 30));
+        countLabel.setMinimumSize(new Dimension(133, 30));
+        countLabel.setPreferredSize(new Dimension(133, 30));
+        countLabel.setMaximumSize(new Dimension(133, 30));
+        titleLabel.setMinimumSize(new Dimension(133, 30));
+        titleLabel.setPreferredSize(new Dimension(133, 30));
+        titleLabel.setMaximumSize(new Dimension(133, 30));
+        descriptionLabel.setMinimumSize(new Dimension(133, 30));
+        descriptionLabel.setPreferredSize(new Dimension(133, 30));
+        descriptionLabel.setMaximumSize(new Dimension(133, 30));
+        cueLabel.setMinimumSize(new Dimension(133, 30));
+        cueLabel.setPreferredSize(new Dimension(133, 30));
+        cueLabel.setMaximumSize(new Dimension(133, 30));
+        executeLabel.setMinimumSize(new Dimension(133, 30));
+        executeLabel.setPreferredSize(new Dimension(133, 30));
+        executeLabel.setMaximumSize(new Dimension(133, 30));
+        headerPanel.add(indexLabel);
+        headerPanel.add(countLabel);
+        headerPanel.add(titleLabel);
+        headerPanel.add(descriptionLabel);
+        headerPanel.add(cueLabel);
+        headerPanel.add(executeLabel);
+        scrollablePanel.add(headerPanel);
         for (int i = 0; i< items.size(); i++){
-            JPanel FV_panel = new JPanel();
-            FV_panel.setLayout(new BoxLayout(FV_panel, BoxLayout.X_AXIS));
-            FV_panel.setPreferredSize(new Dimension(10000, 60));
-            FV_panel.setMaximumSize(new Dimension(10000, 60));
-            FV_panel.setOpaque(false);
-
-            FlowViewItem fv = items.get(i);
-            JLabel flowviewLabel = new JLabel("Trigger: "+ fv.getCount());
-            FV_panel.add(flowviewLabel);
-            FV_panel.add(fv.getTitleLabel());
-            FV_panel.add(fv.getExecuteButton());
-            FV_panel.add(fv.getCountLabel());
-            FV_panel.add(fv.getIndexLabel());
-            scrollablePanel.add(FV_panel);
-            System.out.println("BIGHUGECAPSYOUCANTMISSME AL LALALALALALALA LA LAL LALA ");
+            scrollablePanel.add(Box.createVerticalStrut(5));
+            scrollablePanel.add(items.get(i));
         }
         scrollPane = new JScrollPane(scrollablePanel);
         scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
         scrollPane.getVerticalScrollBar().setUnitIncrement(20);
+        scrollPane.getVerticalScrollBar().addAdjustmentListener(e -> {
+            reinitializeFlowViewPanel();
+        });
         scrollPane.setBackground(new Color(0, 0, 0, 0));
         scrollPane.setVisible(true);
         this.add(scrollPane);
         this.setVisible(true);
     }
 
-    private class FlowViewItem {
+    public void reinitializeFlowViewPanel() {
+        scrollablePanel.removeAll();
+        scrollablePanel.add(headerPanel);
+        for (FlowViewItem fvi : items) {
+            fvi.generateLabels();
+            scrollablePanel.add(fvi);
+        }
+        scrollPane.revalidate();
+        scrollPane.repaint();
+        scrollPane.revalidate();
+        scrollPane.repaint();
+    }
+
+    private class FlowViewItem extends JPanel{
         private int index;
         private int count;
         private String title;
@@ -106,9 +162,9 @@ public class FlowViewGUI extends JPanel {
         public FlowViewItem(int index, int count, String title, String description, String cue) {
             this.index = index;
             this.count = count;
-            this.title = wrap(title);
-            this.description = wrap(description);
-            this.cue = wrap(cue);
+            this.title = title;
+            this.description = description;
+            this.cue = cue;
             this.executeButton = new JButton();
             ImageIcon i = new ImageIcon(ScrubBarGUI.PATH_PLAY_ICON);
             executeButton.setIcon(new ImageIcon(i.getImage().getScaledInstance(16,16, Image.SCALE_SMOOTH)));
@@ -117,9 +173,15 @@ public class FlowViewGUI extends JPanel {
                 System.out.println(index);
                 rfSignalListener.onRFSignal(index);
             });
+            this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+            this.setPreferredSize(new Dimension(800, 50));
+            this.setMaximumSize(new Dimension(800, 50));
+            this.setMinimumSize(new Dimension(800, 50));
+            this.setBackground(new Color(0xE8E8E8));
+            this.setOpaque(true);
         }
 
-        private String wrap(String s) {
+        public static String wrap(String s) {
             for (int i = 0; i < s.length() / 30; i += 30) {
                 while (i < s.length() && s.charAt(i) != ' ') {
                     i++;
@@ -135,11 +197,42 @@ public class FlowViewGUI extends JPanel {
         }
 
         public void generateLabels() {
-            indexLabel = new JLabel(Integer.toString(index+1));
-            countLabel = new JLabel(Integer.toString(count));
-            titleLabel = new JLabel(title);
-            descriptionLabel = new JLabel(description);
-            cueLabel = new JLabel(cue);
+            this.removeAll();
+            indexLabel = new JLabel(wrap(Integer.toString(index+1)));
+            indexLabel.setMinimumSize(new Dimension(133, 30));
+            indexLabel.setPreferredSize(new Dimension(133, 30));
+            indexLabel.setMaximumSize(new Dimension(133, 30));
+            countLabel = new JLabel(wrap(Integer.toString(count)));
+            countLabel.setMinimumSize(new Dimension(133, 30));
+            countLabel.setPreferredSize(new Dimension(133, 30));
+            countLabel.setMaximumSize(new Dimension(133, 30));
+            titleLabel = new JLabel(wrap(title));
+            titleLabel.setMinimumSize(new Dimension(133, 30));
+            titleLabel.setPreferredSize(new Dimension(133, 30));
+            titleLabel.setMaximumSize(new Dimension(133, 30));
+            descriptionLabel = new JLabel(wrap(description));
+            descriptionLabel.setMinimumSize(new Dimension(133, 30));
+            descriptionLabel.setPreferredSize(new Dimension(133, 30));
+            descriptionLabel.setMaximumSize(new Dimension(133, 30));
+            cueLabel = new JLabel(wrap(cue));
+            cueLabel.setMinimumSize(new Dimension(133, 30));
+            cueLabel.setPreferredSize(new Dimension(133, 30));
+            cueLabel.setMaximumSize(new Dimension(133, 30));
+            this.add(indexLabel);
+            this.add(countLabel);
+            this.add(titleLabel);
+            this.add(descriptionLabel);
+            this.add(cueLabel);
+            executeButton.setPreferredSize(new Dimension(40, 40));
+            executeButton.setMaximumSize(new Dimension(40, 40));
+            executeButton.setMinimumSize(new Dimension(40, 40));
+            executeButton.requestFocusInWindow();
+            this.add(executeButton);
+            if (started && (currentTrigger - 1) % items.size() == index) {
+                this.setBackground(new Color(0xDBE6FF));
+            } else {
+                this.setBackground(new Color(0xE8E8E8));
+            }
         }
 
         public JButton getExecuteButton() {
