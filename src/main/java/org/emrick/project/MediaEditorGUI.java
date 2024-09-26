3447b2862d487765708cd0b392220ed626f4371f
JDanison <bucketfoot35@gmail.com>
2024-09-25 23:04:47 -0400

    Allowed for Last Board to be Programmed over wifi.
    Also changed color in programming response matrix to be blue instead of green. It was easier to see.
    
    Progress from 9/25

--- src/main/java/org/emrick/project/MediaEditorGUI.java
+++ src/main/java/org/emrick/project/MediaEditorGUI.java
@@ -1665,7 +1665,7 @@ public class MediaEditorGUI extends Component implements ImportListener, ScrubBa
                     }
                     i++;
                 }
-
+                System.out.println("nothing");
                 for (LEDStrip ledStrip : footballFieldPanel.drill.ledStrips) {
                     for (Effect e : ledStrip.getEffects()) {
                         if (e.getEffectType() == EffectList.GRID) {
